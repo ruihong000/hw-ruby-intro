dde6f3d3302b84929f8b0cf681f0da7a8ef7fe68
EC2 Default User <ec2-user@ip-172-31-31-15.us-east-2.compute.internal>
2019-02-02 03:22:46 +0000

    wrh_hw1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,94 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  s = 0
+  arr.collect{|x| s = s+x}
+  return s
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+
+  arr.sort!
+  return arr.fetch(-1) { |index| 0 }+arr.fetch(-2) { |index| 0 }
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  l1 = 0
+  l2 = 0
+  arr.each do |x| 
+    l2 = 0
+    arr.each do |y|  
+      if x+y == n and l1 != l2
+        return true
+      end
+      l2+=1
+    end
+  l1+=1
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  'Hello, '+name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if  (s =~ /\A[qwrtypsdfghjklzxcvbnm]/i) != nil
+   return true
+  
+  else
+    return false
+  end
 end
 
+
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =="0"
+    return true
+  end
+  a = s.to_f
+  
+  if a == 0
+    return false
+  end
+  if a%4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
-
 class BookInStock
-# YOUR CODE HERE
+  def initialize(startingisbn,startingprice)
+    @isbn = startingisbn
+    @price = startingprice
+    begin 
+      if @price <= 0 or @isbn == ""
+        raise ArgumentError
+      end
+    end
+  end
+  def isbn
+    @isbn
+  end
+  def isbn= x
+    @isbn=x
+  end
+  def price
+    @price
+  end
+  def price= x
+    @price = x
+  end
+  def price_as_string
+    mac = price.to_f.to_s.split(pattern='.')
+    if mac[1].length <2
+      return '$'+price.to_f.to_s+'0'
+    end
+    return '$'+price.to_f.to_s
+  end
+
 end
